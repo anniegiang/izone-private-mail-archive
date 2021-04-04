78776ace01c85cb5e02732360cdfdceb1f8432db
Annie <annieg4123@gmail.com>
2021-04-03 20:23:25 -0700

    change file name & format console text

--- index.js
+++ index.js
@@ -10,16 +10,27 @@ class App {
     this.MailSaver = new MailSaver();
   }
 
+  htmlFileName(mail) {
+    const mailDate = new Date(mail.receive_datetime);
+    const formattedDate = `${mailDate.getFullYear()}-${
+      mailDate.getMonth() + 1
+    }-${mailDate.getDate()}`;
+    return `${mail.id}__${mail.subject_ko}__${formattedDate}__.html`;
+  }
+
   async init() {
     if (!this.checkConfig) {
-      console.log('Your settings are incomplete.');
+      console.log('❗️ Your settings are incomplete.');
       return;
     }
 
     const initialInbox = await this.PMApi.getInbox();
 
     if (initialInbox.error || !initialInbox.data.mails) {
-      console.error('There was an error getting your inbox: ', initialInbox);
+      console.error(
+        '❗️ There was an error getting your inbox: ',
+        initialInbox
+      );
       return;
     }
 
@@ -30,44 +41,46 @@ class App {
     const latestMailPath = path.join(
       directory,
       `${latestMail.member.realname_ko}`,
-      `${latestMail.id}.html`
+      this.htmlFileName(latestMail)
     );
 
     this.MailSaver.makeDirectory(directory);
 
     if (this.MailSaver.directoryExists(latestMailPath)) {
-      console.log(`No new mail, lastest mail is ${latestMail.id}.`);
+      console.log(`✅  No new mail, lastest mail is ${latestMail.id}.`);
       return;
     }
 
     const userProfileResponse = await this.PMApi.getProfile();
 
     if (userProfileResponse.error || !userProfileResponse.data.user) {
-      console.error('Your account was not found, cannot fetch mail.');
+      console.error('❗️ Your account was not found, cannot fetch mail.');
       return;
     }
 
     const { user } = userProfileResponse.data;
     this.MailSaver.setUser(user);
 
-    console.log(`Fetching mails for ${user.nickname}...`);
+    console.log(`💌 Fetching mails for ${user.nickname}...\n`);
 
     let done = false;
     let page = 1;
     let totalMails = 0;
+    let failedMails = 0;
 
     while (!done) {
       const inbox = await this.PMApi.getInbox(page);
 
       if (inbox.error || !inbox.data.mails) {
-        console.error('There was an error getting your inbox: ', inbox);
+        console.error('❗️ There was an error getting your inbox: ', inbox);
         done = true;
         break;
       }
 
       for (const mail of inbox.data.mails) {
+        const htmlFileName = this.htmlFileName(mail);
         const memberDir = path.join(directory, mail.member.realname_ko);
-        const mailPath = path.join(memberDir, `${mail.id}.html`);
+        const mailPath = path.join(memberDir, htmlFileName);
 
         if (this.MailSaver.directoryExists(mailPath)) {
           continue;
@@ -97,9 +110,16 @@ class App {
           mailDetailsHTMLString: mailDetails.data,
         };
 
-        this.MailSaver.saveMail(newMail, mailPath);
-        console.log(`Saved mail: ${mail.id}`);
-        totalMails++;
+        console.log(`📩 Saving ${member.realname_ko} - ${htmlFileName}`);
+        const savedStatus = await this.MailSaver.saveMail(newMail, mailPath);
+
+        if (savedStatus === undefined) {
+          console.log('✅ Saved!\n');
+          totalMails++;
+        } else {
+          console.log('❌ Fail!\n');
+          failedMails++;
+        }
       }
 
       if (!inbox.data.has_next_page) {
@@ -109,9 +129,20 @@ class App {
 
       page++;
     }
+
     console.log(
-      `Finished saving ${totalMails} new ${totalMails > 2 ? 'mails' : 'mail'}!`
+      `🎉 Finished saving ${totalMails} new ${
+        totalMails > 2 ? 'mails' : 'mail'
+      }!`
     );
+
+    if (failedMails) {
+      console.log(
+        `❗️ Failed to save ${failedMails} ${
+          failedMails > 2 ? 'mails' : 'mail'
+        }!`
+      );
+    }
   }
 
   get checkConfig() {
