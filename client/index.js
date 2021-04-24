24e48bbad5260be21d0da9e3a8f41389196e067b
Annie <annieg4123@gmail.com>
2021-04-24 11:44:34 -0700

    encode file path

--- client/index.js
+++ client/index.js
@@ -2,7 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
   const allMails = document.querySelectorAll('.mail');
   for (const mailEL of allMails) {
     mailEL.onclick = function () {
-      window.location.href = mailEL.dataset.src;
+      const mailPath = mailEL.dataset.src;
+
+      const reg = new RegExp('[mib][0-9]{5}');
+      const startIdx = mailPath.search(reg);
+
+      const root = mailPath.slice(0, startIdx);
+      const fileName = mailPath.slice(startIdx);
+      const encoded = encodeURIComponent(fileName);
+
+      window.location.href = root + encoded;
     };
   }
 });
